e1dd57ca7cf86e20c193852970c92804919544e5
levletom <levletom@post.bgu.ac.il>
2018-11-30 17:40:30 +0200

    Merge branches 'Amit' and 'tomer' of https://github.com/levletom/spl191BookStore into tomer
    # Conflicts:
    #	src/main/java/bgu/spl/mics/MessageBusImpl.java

--- src/main/java/bgu/spl/mics/Future.java
+++ src/main/java/bgu/spl/mics/Future.java
@@ -31,8 +31,8 @@ public class Future<T> {
      * @return return the result of type T if it is available, if not wait until it is available.
      * 	       
      */
-	public T get() {
-		synchronized (this) {
+	public synchronized T get() {
+
 			while (result == null) {
 				try {
 					wait();
@@ -41,19 +41,19 @@ public class Future<T> {
 				}
 
 			}
-		}
+
 		return result;
 	}
 
 	/**
 	 * Resolves the result of this Future object.
 	 */
-	public void resolve (T result) {
-		synchronized (this){
+	public synchronized void resolve (T result) {
+
 			this.result=result;
 			isDone = true;
 			notifyAll();
-		}
+
 	}
 	
 	/**
@@ -74,17 +74,15 @@ public class Future<T> {
      * 	       wait for {@code timeout} TimeUnits {@code unit}. If time has
      *         elapsed, return null.
      */
-	public T get(long timeout, TimeUnit unit) {
-		synchronized (this) {
-			if (result == null) {
-				try {
-					unit.wait(timeout);
-				} catch (InterruptedException e) {
+	public synchronized T get(long timeout, TimeUnit unit) {
+		if(result==null){
+			try {
+				wait(unit.toMillis(timeout));
+			} catch (InterruptedException e) {
 
-				}
 			}
-			return result;
 		}
+		return result;
 	}
 
 }
