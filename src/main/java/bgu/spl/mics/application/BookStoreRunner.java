325b886a82b2ee9d04a6590d5621fc973f8d6a46
levletom <levletom@post.bgu.ac.il>
2018-12-18 11:25:48 +0200

    SUBMITEDVERSION

--- src/main/java/bgu/spl/mics/application/BookStoreRunner.java
+++ src/main/java/bgu/spl/mics/application/BookStoreRunner.java
@@ -27,6 +27,8 @@ public class BookStoreRunner {
     private static String booksHashMapFile;
     private static String listOfOrderReceiptsFile;
     private static String moneyRegisterFile;
+
+
     public static void main(String[] args) {
         customerHashMap = new HashMap<>();
         allThreadsExeptTimer = new ConcurrentLinkedQueue<>();
@@ -53,39 +55,10 @@ public class BookStoreRunner {
         checkThreadsAreReady();
         createTimerThread();
         joinAll();
-
         printAlObjects();
-
-
-        //printForYuvalsTestsONLY(args);
-
-
-
     }
 
-    private static void printForYuvalsTestsONLY(String[] args) {
-        int numOfTest = Integer.parseInt(args[0].replace(new File(args[0]).getParent(), "").replace("/", "").replace(".json", ""));
-        String dir = new File(args[1]).getParent() + "/" + numOfTest + " - ";
-        Customer[] customers1 = customerHashMap.values().toArray(new Customer[0]);
-        Arrays.sort(customers1, Comparator.comparing(Customer::getName));
-        String str_custs = customers2string(customers1);
-        str_custs = str_custs.replaceAll(", ", "\n---------------------------\n").replace("[", "").replace("]", "");
-        Print(str_custs, dir + "Customers");
 
-        String str_books = Arrays.toString(Inventory.getInstance().getBooksInventory().values().toArray());
-        str_books = str_books.replaceAll(", ", "\n---------------------------\n").replace("[", "").replace("]", "");
-        Print(str_books, dir + "Books");
-
-        List<OrderReceipt> receipts_lst = MoneyRegister.getInstance().getOrderReceiptsForTestsONLY();
-        receipts_lst.sort(Comparator.comparing(OrderReceipt::getOrderId));
-        receipts_lst.sort(Comparator.comparing(OrderReceipt::getOrderTick));
-        OrderReceipt[] receipts = receipts_lst.toArray(new OrderReceipt[0]);
-        String str_receipts = receipts2string(receipts);
-        str_receipts = str_receipts.replaceAll(", ", "\n---------------------------\n").replace("[", "").replace("]", "");
-        Print(str_receipts, dir + "Receipts");
-
-        Print(MoneyRegister.getInstance().getTotalEarnings() + "", dir + "Total");
-    }
 
     /**
      * checks all initiated thread are in waiting state - meaning they finished registering and are now waithing for messages.
@@ -201,64 +174,6 @@ public class BookStoreRunner {
     }
 
 
-    public static String customers2string(Customer[] customers) {
-        String str = "";
-        for (Customer customer : customers)
-            str += customer2string(customer) + "\n---------------------------\n";
-        return str;
-    }
-
-    public static String customer2string(Customer customer) {
-        String str = "id    : " + customer.getId() + "\n";
-        str += "name  : " + customer.getName() + "\n";
-        str += "addr  : " + customer.getAddress() + "\n";
-        str += "dist  : " + customer.getDistance() + "\n";
-        str += "card  : " + customer.getCreditNumber() + "\n";
-        str += "money : " + customer.getAvailableCreditAmount();
-        return str;
-    }
-
-    public static String books2string(BookInventoryInfo[] books) {
-        String str = "";
-        for (BookInventoryInfo book : books)
-            str += book2string(book) + "\n---------------------------\n";
-        return str;
-    }
-
-    public static String book2string(BookInventoryInfo book) {
-        String str = "";
-        str += "title  : " + book.getBookTitle() + "\n";
-        str += "amount : " + book.getAmountInInventory() + "\n";
-        str += "price  : " + book.getPrice();
-        return str;
-    }
-
-
-    public static String receipts2string(OrderReceipt[] receipts) {
-        String str = "";
-        for (OrderReceipt receipt : receipts)
-            str += receipt2string(receipt) + "\n---------------------------\n";
-        return str;
-    }
-    public static String receipt2string(OrderReceipt receipt) {
-        String str = "";
-        str += "customer   : " + receipt.getCustomerId() + "\n";
-        str += "order tick : " + receipt.getOrderTick() + "\n";
-        str += "id         : " + receipt.getOrderId() + "\n";
-        str += "price      : " + receipt.getPrice() + "\n";
-        str += "seller     : " + receipt.getSeller();
-        return str;
-    }
-
-    public static void Print(String str, String filename) {
-        try {
-            try (PrintStream out = new PrintStream(new FileOutputStream(filename))) {
-                out.print(str);
-            }
-        } catch (IOException e) {
-            System.out.println("Exception: " + e.getClass().getSimpleName());
-        }
-    }
 
 }
 
